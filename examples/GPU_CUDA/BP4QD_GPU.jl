60e5f6c87c09a007f27ba666a372a6eae7acf2f0
gbene <gabri.benedetti@gmail.com>
2026-08-10 15:27:25 +0000

    Full example change using new file placements

--- examples/GPU_CUDA/BP4QD_GPU.jl
+++ examples/GPU_CUDA/BP4QD_GPU.jl
@@ -62,12 +62,12 @@ detector = CatalogDetector(1e-3, 1e-2, experiment, algorithm)
 
 # Additional samplers
 
-# samplers = Array{HighSeas.AbstractSampler, 1}(undef, 14)
-# for sp in 1:14
-#     samplers[sp] = PointSampler("../BP4_sample_points.txt", sp, 700000, experiment)
-# end
-# samplers[15] = SectionSampler("dx", 0.0, "y", 700000, experiment)
-# samplers[16] = ContourSampler("V", 1e-3, experiment)
+samplers = Array{HighSeas.AbstractSampler, 1}(undef, 16)
+for sp in 1:14
+    samplers[sp] = PointSampler("../resources/scec/bp4-qd/BP4_sample_points.txt", sp, 700000, experiment)
+end
+samplers[15] = SectionSampler("dx", 0.0, "y", 700000, experiment)
+samplers[16] = ContourSampler("V", 1e-3, experiment)
 # samplers = [ContourSampler(:V, 1e-3, experiment)]
 
 # Define savers
@@ -76,14 +76,14 @@ stepsaver = StepSaver(500, experiment, algorithm) # save simulation state at eve
 catalogsaver = CatalogSaver(500, experiment, algorithm) # save catalog at every 500 steps
 
 # # snapshotsaver = SnaptshotSaver("BP4QD_out", 100, plotter, experiment, algorithm) # save a figure at every 500 steps
-# samplersaver = SamplerSaver(500, samplers, experiment, algorithm) # save the samplers at every 500 steps
+samplersaver = SamplerSaver(500, samplers, experiment, algorithm) # save the samplers at every 500 steps
 
 
 # savers = [stepsaver, catalogsaver]
 # or
 # savers = [stepsaver, catalogsaver, snapshotsaver]
 #or
-savers = [stepsaver, catalogsaver]
+savers = [stepsaver, catalogsaver, samplersaver]
 #or ...
 
 
@@ -94,10 +94,10 @@ tf = input_dict["tf"]*(365*24*60*60)
 
 # solver = StepSolver(1, savers, detector)
 
-solver = TimeSolver(tf, savers, detector)
+# solver = TimeSolver(tf, savers, detector)
 
 # or add samplers
-# solver = TimeSolver(tf, savers, detector, samplers)
+solver = TimeSolver(tf, savers, detector, samplers)
 
 
 # Solve
